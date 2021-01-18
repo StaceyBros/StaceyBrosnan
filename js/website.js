e6abc8ddfb4780ac534d2c13a7e5ae3a756ab5e7
Stacey Brosnan <staceybrosnan@gmail.com>
2021-01-18 21:24:35 +1100

    message

---
+++ js/website.js
@@ -0,0 +1,36 @@
+
+const sendEmail = function () {
+
+  $( "#searchForm" ).submit(function( event ) {
+
+  // Stop form from submitting normally
+  event.preventDefault();
+
+  // Get some values from elements on the page:
+  const $form = $( this ),
+    term = $form.find( "input[name='s']" ).val(),
+    url = $form.attr( "action" );
+
+  // Send the data using post
+  const posting = $.post( url, { s: term } );
+
+  // Put the results in a div
+  posting.done(function( data ) {
+    const content = $( data ).find( "#content" );
+    $( "#result" ).empty().append( content );
+  });
+});
+
+
+  }
+
+const sentEmail = function () {
+
+}
+
+
+  $(document).ready( function () {
+    $('#submit').on('click', function(){
+      sendEmail();
+    });
+  });
