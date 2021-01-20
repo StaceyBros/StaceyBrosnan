199646db5996186081c84cf71184d950f255712a
Stacey Brosnan <staceybrosnan@gmail.com>
2021-01-21 10:44:50 +1100

    email

--- js/website.js
+++ js/website.js
@@ -4,25 +4,27 @@
 //   event.preventDefault();
 //
   const sendEmail = function () {
-//
-  // $( "#contact" ).submit(function( ) {
-//
-//   // Get some values from elements on the page:
-//   const $form = $( this ),
-//     term = $form.find( "input[name='s']" ).val(),
-//     url = $form.attr( "action" );
-//
-//   // Send the data using post
-//   const posting = $.post( url, { s: term } );
-//
-//   // Put the results in a div
-//   posting.done(function( data ) {
-//     const content = $( data ).find( "#content" );
-//     $( "#result" ).empty().append( content );
-//   });
-// });
-//
-//
+
+
+
+
+  $( "#contact" ).submit(function( ) {
+
+  // Get some values from elements on the page:
+  const $form = $( this ),
+    term = $form.find( "input[name='s']" ).val(),
+    url = $form.attr( "action" );
+
+  // Send the data using post
+  const posting = $.post( url, { s: term } );
+
+  // // Put the results in a div
+  // posting.done(function( data ) {
+  //   const content = $( data ).find( "#content" );
+  //   $( "#result" ).empty().append( content );
+  // });
+});
+
   }
 //
 // const sentEmail = function () {
