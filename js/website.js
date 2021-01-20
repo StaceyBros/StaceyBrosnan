3728d0015151cd7635f72108665a56ee40f26197
Stacey Brosnan <staceybrosnan@gmail.com>
2021-01-21 10:07:37 +1100

    email

--- js/website.js
+++ js/website.js
@@ -1,36 +1,38 @@
-// const sendEmail = function () {
-//
-//   $( "#contact" ).submit(function( event ) {
-//
-//   // Stop form from submitting normally
-//   event.preventDefault();
-//
-//   // Get some values from elements on the page:
-//   const $form = $( this ),
-//     term = $form.find( "input[name='s']" ).val(),
-//     url = $form.attr( "action" );
-//
-//   // Send the data using post
-//   const posting = $.post( url, { s: term } );
-//
-//   // Put the results in a div
-//   posting.done(function( data ) {
-//     const content = $( data ).find( "#content" );
-//     $( "#result" ).empty().append( content );
-//   });
-// });
-//
-//
-//   }
-//
-// const sentEmail = function () {
-//     event.preventDefault();
-// }
-//
-//
-// $(document).ready( function () {
-//     $('#submit').on('click', function(){
-//       sendEmail();
-//       event.preventDefault();
-//     });
-// });
+
+
+  // Stop form from submitting normally
+  event.preventDefault();
+
+  const sendEmail = function () {
+
+  $( "#contact" ).submit(function( event ) {
+
+  // Get some values from elements on the page:
+  const $form = $( this ),
+    term = $form.find( "input[name='s']" ).val(),
+    url = $form.attr( "action" );
+
+  // Send the data using post
+  const posting = $.post( url, { s: term } );
+
+  // Put the results in a div
+  posting.done(function( data ) {
+    const content = $( data ).find( "#content" );
+    $( "#result" ).empty().append( content );
+  });
+});
+
+
+  }
+
+const sentEmail = function () {
+    event.preventDefault();
+}
+
+
+$(document).ready( function () {
+    $('#form').on('click', function(){
+      sendEmail();
+      event.preventDefault();
+    });
+});
