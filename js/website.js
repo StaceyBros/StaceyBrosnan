6e8478574130cf75b546bc1b2d70c5f95f53f771
Stacey Brosnan <staceybrosnan@gmail.com>
2021-01-24 15:56:26 +1100

    sizing

--- js/website.js
+++ js/website.js
@@ -3,45 +3,45 @@
 //   // Stop form from submitting normally
 //   event.preventDefault();
 //
-  const sendEmail = function () {
+  // const sendEmail = function () {
 
 
 
 
-  $( "#contact" ).submit(function( ) {
-
-  // Get some values from elements on the page:
-  const $form = $( this ),
-    term = $form.find( "input[name='s']" ).val(),
-    url = $form.attr( "action" );
-
-  // Send the data using post
-  const posting = $.post( url, { s: term } );
-
-  // // Put the results in a div
-  // posting.done(function( data ) {
-  //   const content = $( data ).find( "#content" );
-  //   $( "#result" ).empty().append( content );
-  // });
-});
-
-  }
+//   $( "#contact" ).submit(function( ) {
 //
-// const sentEmail = function () {
-//     event.preventDefault();
-// }
-
-$(document).ready( function(){
-    $('#form').on('submit', (e) => {
-        e.preventDefault();
-        console.log('clicked');
-        sendEmail();
-    })
-});
-
-// $(document).ready( function () {
-//     $('#form').on('click', function(){
-//       event.preventDefault();
-//       // sendEmail();
-//     });
+//   // Get some values from elements on the page:
+//   const $form = $( this ),
+//     term = $form.find( "input[name='s']" ).val(),
+//     url = $form.attr( "action" );
+//
+//   // Send the data using post
+//   const posting = $.post( url, { s: term } );
+//
+//   // // Put the results in a div
+//   // posting.done(function( data ) {
+//   //   const content = $( data ).find( "#content" );
+//   //   $( "#result" ).empty().append( content );
+//   // });
 // });
+//
+//   }
+// //
+// // const sentEmail = function () {
+// //     event.preventDefault();
+// // }
+//
+// $(document).ready( function(){
+//     $('#form').on('submit', (e) => {
+//         e.preventDefault();
+//         console.log('clicked');
+//         sendEmail();
+//     })
+// });
+//
+// // $(document).ready( function () {
+// //     $('#form').on('click', function(){
+// //       event.preventDefault();
+// //       // sendEmail();
+// //     });
+// // });
